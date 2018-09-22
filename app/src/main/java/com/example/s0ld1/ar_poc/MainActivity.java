a9a1b21ccdd6ddb04186882cc6b1b904e775710e
Schadenfreude <s0ld13rBG@gmail.com>
2018-09-22 11:32:27 +0300

    Adding models to planes

--- app/src/main/java/com/example/s0ld1/ar_poc/MainActivity.java
+++ app/src/main/java/com/example/s0ld1/ar_poc/MainActivity.java
@@ -1,33 +1,58 @@
 package com.example.s0ld1.ar_poc;
 
 import android.Manifest;
-import android.app.ActivityManager;
-import android.content.Context;
-import android.os.Build;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.s0ld1.ar_poc.utils.ARUtils;
-import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Anchor;
 import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
 import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
 import com.google.ar.core.exceptions.CameraNotAvailableException;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
 import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final boolean DEBUG = false;
 
-    private ArSceneView arSceneView;
     private boolean isArInstalled = false;
+    private boolean hasFinishedLoading = false;
+    private Snackbar loadingSnackbar;
+
+    private ArSceneView arSceneView;
+    private ModelRenderable andyRenderable;
+    private ModelRenderable distilRenderable;
+    private ModelRenderable hayRenderable;
+    private ViewRenderable menuRenderable;
+
+    private GestureDetector gestureDetector;
+    private AnchorNode menuAnchorNode;
+    private Anchor menuAnchor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +66,43 @@ public class MainActivity extends AppCompatActivity {
 
         ARUtils.checkForARSupport(this, TAG);
         ARUtils.requestARInstall(this, isArInstalled);
+
+        loadModels();
+
+        arSceneView.getScene().addOnUpdateListener(frameTime -> {
+            if (loadingSnackbar == null) {
+                return;
+            }
+
+            final Frame frame = arSceneView.getArFrame();
+            if (frame == null) {
+                return;
+            }
+
+            if (frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
+                return;
+            }
+
+            for (Plane plane : frame.getUpdatedTrackables(Plane.class)) {
+                if (plane.getTrackingState() == TrackingState.TRACKING) {
+                    hideLoadingMessage();
+                    return;
+                }
+            }
+        });
+
+        gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                onSceneTapped(e);
+                return false;
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
     }
 
     @Override
@@ -72,6 +134,17 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Unable to get camera", Toast.LENGTH_SHORT).show();
             finish();
         }
+
+        arSceneView
+                .getScene()
+                .setOnTouchListener(
+                        (HitTestResult hitTestResult, MotionEvent event) -> {
+                            gestureDetector.onTouchEvent(event);
+                            // Return false so touch events are also transmitted to the scene
+                            return false;
+                        });
+
+        showLoadingMessage();
     }
 
     @Override
@@ -107,5 +180,128 @@ public class MainActivity extends AppCompatActivity {
                                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
+    }
+
+    private void loadModels() {
+        final CompletableFuture<ModelRenderable> andy = ModelRenderable.builder()
+                .setSource(this, Uri.parse("andy.sfb")).build();
+        final CompletableFuture<ModelRenderable> distillery = ModelRenderable.builder()
+                .setSource(this, Uri.parse("distillery.sfb")).build();
+        final CompletableFuture<ModelRenderable> hay = ModelRenderable.builder()
+                .setSource(this, Uri.parse("hay.sfb")).build();
+        final CompletableFuture<ViewRenderable> menuStage =
+                ViewRenderable.builder().setView(this, R.layout.view_menu).build();
+
+        CompletableFuture.allOf(
+                andy,
+                distillery,
+                hay,
+                menuStage
+        ).handle((notUsed, throwable) -> {
+            try {
+                andyRenderable = andy.get();
+                distilRenderable = distillery.get();
+                hayRenderable = hay.get();
+                menuRenderable = menuStage.get();
+
+                menuRenderable.getView().findViewById(R.id.item_andy).setOnClickListener(this::onMenuItemClicked);
+                menuRenderable.getView().findViewById(R.id.item_distillery).setOnClickListener(this::onMenuItemClicked);
+                menuRenderable.getView().findViewById(R.id.item_hay).setOnClickListener(this::onMenuItemClicked);
+
+                hasFinishedLoading = true;
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        });
+    }
+
+    private void showLoadingMessage() {
+        if (loadingSnackbar == null || !loadingSnackbar.isShownOrQueued()) {
+            loadingSnackbar =
+                    Snackbar.make(
+                            this.findViewById(android.R.id.content),
+                            R.string.plane_finding,
+                            Snackbar.LENGTH_INDEFINITE);
+            loadingSnackbar.show();
+        }
+    }
+
+    private void hideLoadingMessage() {
+        if (loadingSnackbar != null) {
+            loadingSnackbar.dismiss();
+            loadingSnackbar = null;
+        }
+    }
+
+    private void onSceneTapped(MotionEvent tap) {
+        if (!hasFinishedLoading) {
+            return;
+        }
+
+        final Frame frame = arSceneView.getArFrame();
+        if (frame != null && tap != null
+                && frame.getCamera().getTrackingState() == TrackingState.TRACKING) {
+            for (HitResult hit : frame.hitTest(tap)) {
+                final Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane
+                        && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    displayMenu(hit.createAnchor());
+                }
+            }
+        }
+    }
+
+    private void onMenuItemClicked(View view) {
+        if (menuAnchor != null) {
+            switch (view.getId()) {
+                case R.id.item_distillery:
+                    renderNodeForAnchor(distilRenderable, menuAnchor);
+                    break;
+                case R.id.item_hay:
+                    renderNodeForAnchor(hayRenderable, menuAnchor);
+                    break;
+                default:
+                    renderNodeForAnchor(andyRenderable, menuAnchor);
+                    break;
+            }
+            // We don't have a menu displayed anymore
+            menuAnchor = null;
+            menuAnchorNode.setEnabled(false);
+        }
+    }
+
+    private void displayMenu(Anchor anchor) {
+        // Make sure we don't have more than one Menu displayed at a time
+        if (menuAnchor != null) {
+            menuAnchor.detach();
+        }
+        // Mark that we've displayed the menu
+        menuAnchor = anchor;
+
+        if (menuAnchorNode == null) {
+            // Actually render the menu
+            Node node = new Node();
+            node.setRenderable(menuRenderable);
+
+            menuAnchorNode = new AnchorNode();
+            menuAnchorNode.setParent(arSceneView.getScene());
+            menuAnchorNode.addChild(node);
+
+        }
+        menuAnchorNode.setAnchor(anchor);
+        menuAnchorNode.setEnabled(true);
+    }
+
+    private Node renderNodeForAnchor(Renderable renderable, Anchor anchor) {
+        final Node node = new Node();
+        node.setRenderable(renderable);
+
+        final AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arSceneView.getScene());
+        anchorNode.addChild(node);
+
+        return node;
     }
 }
